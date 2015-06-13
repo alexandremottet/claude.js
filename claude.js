2d5124097a1604ce790910000617ad6ca98fac76
Alexandre MOTTET <mottetalexandre@gmail.com>
2015-06-13 14:36:59 +0200

    fix

--- claude.js
+++ claude.js
@@ -4,54 +4,56 @@ var child_processes = require('child_process');
 
 var windows = (os.platform().match("^win") != null);
 
-function sync(device) {
+function sync(device)
+{
     console.log("coucou " + device); 
     tokenFound = true;
 }
 
 function parseToken(device, callback) {
     console.log(device);
-    fs.readdir(device+'/', function(err,files) {
-        if(err == null)
+    var files = fs.readdirsync(device+'/');
+    files.forEach(function(file_name, index, array) {
+        if(file_name == 'autosync-token' && fs.statSync(device+'/autosync-token').isFile() )
         {
-            files.forEach(function(file_name, index, array) {
-                if(file_name == 'autosync-token' && fs.statSync(device+'/autosync-token').isFile() )
-                {
-                    callback(device);
-                }
-            });
+            callback(device);
         }
     });
 }
 
-function parseDevices(callback) {
+function parseDevices(checkDevice, callback)
+{
     if(windows) {
         child_processes.exec('wmic logicaldisk get name', function(err, stdout, stderr)
         {
             array = stdout.split("\r\r\n");
             volumes = array.slice(1, array.length-2);
             volumes.forEach(function(o,i,a) {
-                callback(o.trim())
+                checkDevice(o.trim())
             });
+            callback();
         });
     } else {
+        console.log('process');
         callback();
     }
 }
 
 var tokenFound = false;
-function detectDevices(endCallback) {
-    parseDevices(function() {
-        if(!tokenFound) { 
+function detectDevices(checkDevice) {
+    parseDevices(checkDevice, function parseDeviceCallback() {
+        if(!tokenFound) {
             console.log('token not found');
-            setTimeout(detectDevices, 2000);  
+            setTimeout(function() {
+                detectDevices(checkDevice);
+            }, 500);   
         }
     });
 }
 
-detectDevices(function(device) {
-    parseToken(device, sync) }
-);
+detectDevices(function parseToken(device){
+    parseToken(device, sync);
+});
 
 
 
